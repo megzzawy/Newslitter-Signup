01f47d031816d11b2485a64f40506de59242fb03
megzzawy <amgadsameh99@gmail.com>
2025-03-23 07:02:11 +0200

    fix all styling issues

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,36 +18,36 @@ const Form = ({setEmail,setIsSubmitted}) => {
 
   return (
     <div className='md:flex md:w-3/5 bg-white md:p-4 md:rounded-3xl  shadow-lg md:h-4/5 max-md:grid max-md:justify-center gap-4 md:flex-row-reverse'>
-      <div className='md:w-1/2 flex max-md:self-start relative'>
+      <div className='md:w-1/2 flex max-md:self-start relative justify-end'>
       <img src={MobileImg} alt="illustration" className="w-full md:hidden" />
-      <img src={img} alt="illustration" className="hidden md:block w-full h-[390px] absolute left-7" />
+      <img src={img} alt="illustration" className="hidden md:block h-full w-auto object-cover " />
       </div>
-      <div className='md:w-1/2 grid  justify-center pl-8 h-full content-center gap-3 place-content-center'>
-      <p className='text-black font-semibold md:text-[40px] max-md:text-3xl'>Stay updated!</p>
-      <p className='text-gray-500 text-xs'>Join 60,000+ product managers receiving monthly updates on:</p>
-        <div className='flex items-center text-gray-500 text-xs gap-3'>
+      <div className='md:w-1/2 grid  justify-center pl-8 h-full content-center gap-5 place-content-center'>
+      <p className='text-black font-semibold md:text-[48px] max-md:text-3xl'>Stay updated!</p>
+      <p className='text-gray-500 text-sm'>Join 60,000+ product managers receiving monthly updates on:</p>
+        <div className='flex items-center text-gray-500 text-sm gap-3'>
         <img src={CheckIcon} className='h-4'/>
         <p>Product discovery and building what matters</p>
         </div>
-        <div className='flex items-center text-gray-500 text-xs gap-3'>
+        <div className='flex items-center text-gray-500 text-sm gap-3'>
         <img src={CheckIcon} className='h-4' />
         <p> Measuring to ensure updates are a success</p>
         </div>
-        <div className='flex items-center text-gray-500 text-xs gap-3'>
+        <div className='flex items-center text-gray-500 text-sm gap-3'>
             <img src={CheckIcon} className='h-4'/>
             <p>And much more!</p>
         </div>
 
         <div className='flex justify-between'>
-        <p className='text-black text-xs'>Email address</p>
-        {!isValid && <p className="text-red-500 text-xs mt-1 ">Valid email required</p>}
+        <p className='text-black text-sm'>Email address</p>
+        {!isValid && <p className="text-red-500 text-sm mt-1 ">Valid email required</p>}
         </div>
-        <input id='email' value={emailInput} type='email' className='md:w-full max-md:w-11/12 p-2 text-sm rounded-lg border border-gray-300 text-gray-300 pl-5 invalid:not-placeholder-shown:border-red-500 invalid:not-placeholder-shown:text-gray-500 focus:ring-0 focus:outline-none invalid:bg-red-100' placeholder='email@company.com' onChange={(e) => {
+        <input id='email' value={emailInput} type='email' className='md:w-full max-md:w-11/12 p-4 text-sm rounded-lg border border-gray-300 text-gray-300 pl-5 invalid:not-placeholder-shown:border-red-500 invalid:not-placeholder-shown:text-gray-500 focus:ring-0 focus:outline-none invalid:bg-red-100' placeholder='email@company.com' onChange={(e) => {
             setEmailInput(e.target.value);
             setIsValid(true); // Reset error on change
           }}/>
         
-        <button className='md:w-full max-md:w-11/12 bg-black text-white p-2 px-3 rounded-lg mt-2 text-[14px] cursor-pointer max-md:mb-3' onClick={handleSubmit}>Subscribe to monthly newsletter</button>
+        <button className='md:w-full max-md:w-11/12 bg-black hover:shadow-md shadow-amber-500/70 transition-all duration-200 mt-2 hover:bg-linear-to-r hover:from-red-400 hover:to-orange-300 text-white p-4 px-3 rounded-lg mt-2 text-[14px] cursor-pointer max-md:mb-3' onClick={handleSubmit}>Subscribe to monthly newsletter</button>
 
       </div>
 
--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -5,12 +5,12 @@ const Thanks = ({email,setIsSubmitted}) => {
   return (
     <div className='bg-white w-3/12 h-7/12 rounded-3xl shadow-lg p-8 grid '>
         <img src={SuccesessIcon} className='w-10' />
-      <h1 className='font-bold text-3xl'>Thanks for subscribing!</h1>
+      <h1 className='font-bold text-5xl'>Thanks for subscribing!</h1>
 
-        <p className='text-[10px]'>A confirmation email has been sent to <strong> {email}</strong>. 
+        <p className='text-[14px]'>A confirmation email has been sent to <strong> {email}</strong>. 
         Please open it and click the button inside to confirm your subscription.</p>
 
-        <button className='text-white bg-linear-to-r from-red-600 to-orange-400 rounded-md cursor-pointer hover:shadow-md shadow-amber-500/70 transition-all duration-200 mt-2 hover:bg-linear-to-r hover:from-red-400 hover:to-orange-300' onClick={()=>{setIsSubmitted(false)}}>Dismiss message</button>
+        <button className='text-white bg-black rounded-md cursor-pointer hover:shadow-md shadow-amber-500/70 transition-all duration-200 mt-2 hover:bg-linear-to-r hover:from-red-400 hover:to-orange-300' onClick={()=>{setIsSubmitted(false)}}>Dismiss message</button>
     </div>
   )
 }
